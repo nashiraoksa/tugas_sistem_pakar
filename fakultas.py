61f6fdee8df19b052b486c21362f4e19267d89d3
nashiraoksani <nashiraaoksani@mail.ugm.ac.id>
2023-03-30 12:28:19 +0700

    add rules

--- fakultas.py
+++ fakultas.py
@@ -13,6 +13,17 @@ template_string = """
 env.build(template_string)
 template = env.find_template('fakultas')
 
+template.assert_fact(nama='Biologi',tahun_berdiri=1955,jumlah_prodi=3, klaster='Saintek')
+template.assert_fact(nama='Matematika dan Ilmu Pengetahuan Alam',tahun_berdiri=1955,jumlah_prodi=16, klaster='Saintek')
+template.assert_fact(nama='Geografi',tahun_berdiri=1963,jumlah_prodi=3, klaster='Saintek')
+template.assert_fact(nama='Teknik',tahun_berdiri=1946,jumlah_prodi=14, klaster='Saintek')
+
+template.assert_fact(nama='Ekonomika dan Bisnis',tahun_berdiri=1955,jumlah_prodi=14,klaster='Soshum')
+template.assert_fact(nama='Filsafat',tahun_berdiri=1967,jumlah_prodi=1,klaster='Soshum')
+template.assert_fact(nama='Hukum',tahun_berdiri=1946,jumlah_prodi=1,klaster='Soshum')
+template.assert_fact(nama='Ilmu Budaya',tahun_berdiri=1946,jumlah_prodi=21,klaster='Soshum')
+template.assert_fact(nama='Ilmu Sosial dan Ilmu Politik',tahun_berdiri=1955,jumlah_prodi=17,klaster='Soshum')
+
 template.assert_fact(nama='Kedokteran Hewan',tahun_berdiri=1949,jumlah_prodi=1, klaster='Agro')
 template.assert_fact(nama='Kehutanan',tahun_berdiri=1963,jumlah_prodi=1, klaster='Agro')
 template.assert_fact(nama='Pertanian',tahun_berdiri=1946,jumlah_prodi=9, klaster='Agro')
@@ -23,51 +34,47 @@ template.assert_fact(nama='Kedokteran, Kesehatan Masyarakat, dan Keperawatan',ta
 template.assert_fact(nama='Kedokteran Gigi',tahun_berdiri=1948,jumlah_prodi=3, klaster='Kesehatan')
 template.assert_fact(nama='Farmasi',tahun_berdiri=1946,jumlah_prodi=6, klaster='Kesehatan')
 
-# print(fact1['tahun_berdiri'])
-# print(type(fact1['tahun_berdiri']))
-
-class Fakultas:
-  def __init__(self, nama, tahun):
-    self.nama = nama
-    self.tahun = tahun
-
-test = []
-
-# for fact in env.facts():
-#     if fact.template.name == 'fakultas':
-#         test.append(fact)
-
-# print(test)
+fakultasDict = []
 
 for fact in env.facts():
-    test.append({
+    fakultasDict.append({
        "nama": fact['nama'],
-       "tahun": fact['tahun_berdiri']
+       "tahun": fact['tahun_berdiri'],
+       "prodi": fact['jumlah_prodi'],
+       "klaster": fact['klaster']
        })
 
-nama1 = test[0]['nama']
-nama2 = test[1]['nama']
-tahun1 = test[0]['tahun']
-tahun2 = test[1]['tahun']
-
-print(nama1)
 
 # ====== RULES ======
-rule1 = """
-(defrule k_kesehatan
-  (fakultas (nama ?nama) (klaster "Kesehatan"))
-  =>
-  (printout t "Fakultas " ?nama " termasuk klaster Kesehatan" crlf))
-"""
-env.build(rule1)
+RULES = [ 
+   """
+  (defrule k_kesehatan
+    (fakultas (nama ?nama) (klaster "Kesehatan"))
+    =>
+    (printout t "Fakultas " ?nama " termasuk klaster Kesehatan" crlf))
+  """,
+  """
+  (defrule k_agro
+    (fakultas (nama ?nama) (klaster "Agro"))
+    =>
+    (printout t "Fakultas " ?nama " termasuk klaster Agro" crlf))
+  """,
+  """
+  (defrule k_saintek
+    (fakultas (nama ?nama) (klaster "Saintek"))
+    =>
+    (printout t "Fakultas " ?nama " termasuk klaster Saintek" crlf))
+  """,
+  """
+  (defrule k_soshum
+    (fakultas (nama ?nama) (klaster "Soshum"))
+    =>
+    (printout t "Fakultas " ?nama " termasuk klaster Soshum" crlf))
+  """
+]
 
-rule2 = """
-(defrule k_agro
-  (fakultas (nama ?nama) (klaster "Agro"))
-  =>
-  (printout t "Fakultas " ?nama " termasuk klaster Agro" crlf))
-"""
-env.build(rule2)
+for rule in RULES:
+   env.build(rule)
 
 # ====== FUNCTIONS ======
 def older_faculty(n1, n2, t1, t2):
@@ -79,7 +86,7 @@ def older_faculty(n1, n2, t1, t2):
 env.define_function(older_faculty)
 env.eval('(older_faculty "Kedokteran Hewan" "ASHKDAHS" 1946 1955)')
 
-older_faculty(test[0]['nama'], test[1]['nama'], test[0]['tahun'], test[1]['tahun']) #salah?:(
+older_faculty(fakultasDict[0]['nama'], fakultasDict[1]['nama'], fakultasDict[0]['tahun'], fakultasDict[1]['tahun']) #salah?:(
 
 env.run()
 
